3286753685db253c0b7d070f54872cb130e9937c
Gormly <daniel@sensavation.com>
2021-04-10 14:45:57 +1000

    icons

--- public/editor.js
+++ public/editor.js
@@ -36,7 +36,11 @@ Vue.component("top-nav", {
               hide: editing != 'CARDS' || numimages == numrequired
             }"
           >
-            Add a Photo!
+            Add a Photo
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" class="bi bi-image" viewBox="0 0 16 16">
+  <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+  <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z"/>
+</svg>
           </button>
           <span v-bind:class="{ 
             hide: editing != 'CROP' 
@@ -107,7 +111,10 @@ Vue.component("bottom-nav", {
             v-on:click="clearPhotos"
             v-bind:class="{ hidden: editing == 'CROP' }"
           >
-            Clear Photos
+            Delete All Photos
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="bi bi-trash-fill" viewBox="0 0 16 16">
+  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+</svg>
           </button>
           <span>
             <button
